aa58289500339d68a4b0f83cba02c81b87827dd9
JustinWeicht <justinweicht11@gmail.com>
2021-11-04 17:19:10 -0400

    added links to projects

--- justin-weicht/src/components/PhotoList/index.js
+++ justin-weicht/src/components/PhotoList/index.js
@@ -1,74 +1,72 @@
 import React, { useState } from 'react';
-import Modal from '../Modal';
 
 const PhotoList = ({ category }) => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [currentPhoto, setCurrentPhoto] = useState();
 
   const [photos] = useState([
     {
       name: 'Airplane',
       category: 'work',
-      description: 'Bardic'
+      description: 'Bardic',
+      link: "http://bardic.herokuapp.com/"
     },
     {
       name: 'Books',
       category: 'work',
-      description: 'Booklyst'
+      description: 'Booklyst',
+      link: "https://booklyst-project.herokuapp.com/"
     },
     {
       name: 'Food',
       category: 'work',
-      description: 'Nutriquotient'
+      description: 'Nutriquotient',
+      link: "https://foodisfuel.github.io/gp5_project1/index.html"
     },
     {
       name: 'Money',
       category: 'work',
-      description: 'Budget Tracker'
+      description: 'Budget Tracker',
+      link: "https://module-19-budget-tracker.herokuapp.com/"
     },
     {
       name: 'Blog',
       category: 'work',
-      description: 'Tech Blog'
+      description: 'Tech Blog',
+      link: "https://module-14-tech-blog.herokuapp.com/"
     },
     {
       name: 'Pizza',
       category: 'work',
-      description: 'Pizza Hunt'
+      description: 'Pizza Hunt',
+      link: "https://creepy-nightmare-85054.herokuapp.com/"
     },
     {
       name: 'Checklist',
       category: 'work',
-      description: 'Taskmaster Pro'
+      description: 'Taskmaster Pro',
+      link: "https://justinweicht.github.io/taskmaster-pro/"
     },
     {
       name: 'Runners',
       category: 'work',
-      description: 'Run Buddy'
+      description: 'Run Buddy',
+      link: "https://JustinWeicht.github.io/run-buddy/"
     }
   ]);
 
   const currentPhotos = photos.filter(photo => photo.category === category);
 
-  const toggleModal = (image, i) => {
-    setCurrentPhoto({ ...image, index: i });
-    setIsModalOpen(!isModalOpen);
-  };
-
   return (
     <div>
-      {isModalOpen && (
-        <Modal onClose={toggleModal} currentPhoto={currentPhoto} />
-      )}
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
-          <img
-            src={require(`../../assets/small/${category}/${i}.jpg`).default}
-            alt={image.name}
-            className="img-thumbnail"
-            onClick={() => toggleModal(image, i)}
-            key={image.name}
-          />
+          <a className="img-thumbnail" href={image.link} rel="noreferrer" target="_blank">
+            <img
+              src={require(`../../assets/small/${category}/${i}.jpg`).default}
+              alt={image.name}
+              className=""
+              key={image.name}
+            />
+          </a>
         ))}
       </div>
     </div>
